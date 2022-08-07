df825b2ce36cc7200291d98714544399b428a3ea
ProGor <gorog.veronika@gmail.com>
2022-08-07 21:58:35 +0200

    add inserts.sql

---
+++ html/parcel_number/mysql/inserts.sql
@@ -0,0 +1,34 @@
+-- table email_addresses
+-- insert some examples
+
+insert into email_addresses values (default, 'gorog.veronika@gmail.com');
+insert into email_addresses values (default, 'atad.tigyi@gmail.com');
+insert into email_addresses values (default, 'abigel.tigyi@gmail.com');
+insert into email_addresses values (default, 'a@b.c');
+insert into email_addresses values (default, 'ccc@akarmi.vmi');
+
+-- table partial_numbers
+-- insert some examples and some real interested field for me
+
+insert into partial_numbers values (default, '2059'); 
+insert into partial_numbers values (default, '0503/2'); 
+insert into partial_numbers values (default, '0271/33'); 
+insert into partial_numbers values (default, '02079/84'); 
+insert into partial_numbers values (default, '02079/85'); 
+insert into partial_numbers values (default, '02079/107'); 
+insert into partial_numbers values (default, '02079/78'); 
+insert into partial_numbers values (default, '02079/79'); 
+insert into partial_numbers values (default, '02079/87'); 
+
+-- table email_pnumber_connections
+
+insert into  email_pnumber_connections values (default, 1, 1);  
+insert into  email_pnumber_connections values (default, 1, 2);  
+insert into  email_pnumber_connections values (default, 1, 3);  
+insert into  email_pnumber_connections values (default, 2, 4);  
+insert into  email_pnumber_connections values (default, 2, 5);  
+insert into  email_pnumber_connections values (default, 3, 6);  
+insert into  email_pnumber_connections values (default, 3, 7);  
+insert into  email_pnumber_connections values (default, 3, 8);  
+insert into  email_pnumber_connections values (default, 3, 9);  
+insert into  email_pnumber_connections values (default, 5, 1);  
